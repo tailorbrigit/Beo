14df982ffaddf4dd427cc32882344f334ea156a6
Szabó <tailorbrigit@gmail.com>
2022-04-04 11:28:11 +0200

    PHP nyelv konverziói

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php
+$szam=35;
+$nev="Bringa";
+echo "<!DOCTYPE html>\n";
+echo "<html>";
+echo "<body>";
+echo "alag\nsor<br>";
+echo "alag<br>sor";
+echo "<br>";
+echo "<Típus:>".gettype($szam);
 
-echo file_get_contents('templates/head.tpl');
-echo file_get_contents('templates/nav.tpl');
-echo file_get_contents('templates/foot.tpl');
-echo file_get_contents('templates/home.tpl');
-
-
-
-
-
-
+echo <<<KISKUTYA
+<ul>
+    <li>alma</li>
+    <li>körte</li>
+</ul>
+KISKUTYA;
+echo "<br>PPHP_OS: " . PHP_OS;
+const MIN_KOR=18;
+echo "<br>Jöhet: " .MIN_KOR;
+printf("<br>%d", 58);
+echo "<br>";
+$szog = 1;
+echo sin($szog *pi()/180);
+echo "<br>";
+echo pow(2,8);
+echo "<br>";
+echo rand(1,6);
+echo "<br>";
+$szamStr = "35";
+echo $szamStr +2;
+echo "</body></html>";
 
 
 ?>
